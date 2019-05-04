139d5f2f77bf78e0b6ca2ab9c4e22f7f6f765559
ralracish <carlarblackman@gmail.com>
2019-05-04 09:50:01 -0400

    added Google API key

---
+++ client/src/util/GoogleBooksAPI.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const APIKEY = "AIzaSyBsEwQAb_DHX2K62w1NddCvDaJn - p96Uko";
+
+export default {
+    search: function (query) {
+        let q = (query).split(" ").join("+");
+        return axios.get(BASEURL + q + APIKEY)
+    }
+};
