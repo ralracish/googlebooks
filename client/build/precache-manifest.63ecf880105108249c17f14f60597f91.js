ad32ed07626bfa3603b4fb4411d8c8191e328e10
ralracish <carlarblackman@gmail.com>
2019-05-08 15:18:41 -0400

    build

---
+++ client/build/precache-manifest.63ecf880105108249c17f14f60597f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a47147953220e0be38f3",
+    "url": "/static/js/main.209f62f6.chunk.js"
+  },
+  {
+    "revision": "5f2deaa239853ab38cc2",
+    "url": "/static/js/2.64bdf778.chunk.js"
+  },
+  {
+    "revision": "a47147953220e0be38f3",
+    "url": "/static/css/main.3266ff92.chunk.css"
+  },
+  {
+    "revision": "115f1a9d5ab984148c187a0f6d627d29",
+    "url": "/index.html"
+  }
+];
